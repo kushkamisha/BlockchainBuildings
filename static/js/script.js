89162d6b8f9a5e60105ba8e9f215782f273399a2
Misha <kushkamisha@yahoo.com>
2017-09-24 07:03:33 +0000

    Work is done

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,14 @@
+// $('.loaderImage').hide();
+
 $('#one').click(function() {
+	$('.loaderImage').show();
     $.ajax({
 		url: '/house1',
 		type: 'POST',
 		success: function(response){
-			alert('Success: ' + response);
+			// alert('Success: ' + response);
 			$('#diagram').attr('src', '/static/img/house1.png');
+			$('.loaderImage').hide();
 		},
 		error: function(error){
 			console.log('Error: ' + error);
@@ -12,12 +16,14 @@ $('#one').click(function() {
 	});
 })
 $('#two').click(function() {
+	$('.loaderImage').show();
     $.ajax({
 		url: '/house2',
 		type: 'POST',
 		success: function(response){
-			alert('Success: ' + response);
+			// alert('Success: ' + response);
 			$('#diagram').attr('src', '/static/img/house2.png');
+			$('.loaderImage').hide();
 		},
 		error: function(error){
 			console.log('Error: ' + error);
@@ -25,12 +31,14 @@ $('#two').click(function() {
 	});
 })
 $('#three').click(function() {
+	$('.loaderImage').show();
     $.ajax({
 		url: '/house3',
 		type: 'POST',
 		success: function(response){
-			alert('Success: ' + response);
+			// alert('Success: ' + response);
 			$('#diagram').attr('src', '/static/img/house3.png');
+			$('.loaderImage').hide();
 		},
 		error: function(error){
 			console.log('Error: ' + error);
